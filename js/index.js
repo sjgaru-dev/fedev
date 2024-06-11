32a17c67670fec5c6efdfd75a7aa3e097e077659
sjgaru-dev <sjgaru@gmail.com>
2024-06-12 02:41:47 +0900

    060401

--- js/index.js
+++ js/index.js
@@ -1,1 +1,18 @@
-console.log("Hello World!");
+console.log("Start");
+
+function app() {
+  const btnSave = document.querySelector("#btnSave");
+  const message = document.querySelector(".message");
+
+  function onSave() {
+    console.log("clicked!");
+    message.innerText = "SAVED!";
+    setTimeout(() => {
+      message.innerText = "";
+    }, 1000);
+  }
+
+  btnSave.addEventListener("click", onSave);
+}
+
+document.addEventListener("DOMContentLoaded", app);
