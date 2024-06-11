a75d2bfb58f7eb009446f3eed4b20b9f725cd02a
sjgaru-dev <sjgaru@gmail.com>
2024-06-12 03:15:30 +0900

    end

--- js/index.js
+++ js/index.js
@@ -1,3 +1,4 @@
+// 1 //
 console.log("Start");
 
 function app() {
@@ -16,3 +17,50 @@ function app() {
 }
 
 document.addEventListener("DOMContentLoaded", app);
+
+// 2 //
+
+function Person(name, age) {
+  this.name = name;
+  this.age = age;
+  this.getName = () => this.name;
+}
+
+class Man {
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+  }
+}
+
+const p = new Person("John", 30);
+console.log(p);
+console.log(p.getName());
+
+const p1 = new Man("abc", 24);
+console.log(p1);
+
+// 3 //
+
+function myKey() {
+  let a = 10;
+  function add10() {
+    a += 10;
+    return a;
+  }
+  function min10() {
+    a -= 10;
+    return a;
+  }
+
+  return {
+    add10: add10,
+    min10: min10,
+  };
+}
+
+const key = myKey();
+console.log(key.min10());
+console.log(key.add10());
+console.log(key.add10());
+console.log(key.min10());
